ff8e88b243ffda510a4f7a01c455a9a73f15be1e
sahil <sahil.khanna@nativerank.com>
2020-04-28 21:00:28 -0600

    add sftp commands

--- index.js
+++ index.js
@@ -39,10 +39,11 @@ async function configureHost(args) {
 	try {
 		const sshFolder = `${process.env['HOME']}/.ssh`;
 		await exec.exec(`mkdir -v -p ${sshFolder}`);
+		await writeFileAsync(`${sshFolder}/id_rsa`, args.ssh_private_key);
 		await exec.exec(`chmod 700 ${sshFolder}`);
 		await writeFileAsync(`${sshFolder}/known_hosts`, '');
 		await exec.exec(`chmod 755 ${sshFolder}/known_hosts`);
-
+		await writeFileAsync(`${process.env['HOME']}/sftp_command`, `put ${args.package_name}.zip ${args.remote_path}`);
 		console.log("✅ Configured known_hosts");
 	} catch (error) {
 		console.error("⚠️ Error configuring known_hosts");
@@ -57,10 +58,11 @@ async function configureHost(args) {
 async function syncFiles(args) {
 	try {
 		await core.group("Compressing files", async () => {
-
 			let result = await exec.exec(`git archive -o ${args.package_name}.zip HEAD`);
 			console.log(result)
 			result = await exec.exec(`ls -l`);
+			console.log(result)
+			result = await exec.exec(`sftp -b ${process.env['HOME']}/sftp_command -P ${args.port} -o StrictHostKeyChecking=no ${args.username}@${args.server}`);
 			console.log(result)
 			return result;
 		});
